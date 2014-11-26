839ca858874ac0068fde7c6da138bce490273c43
gudata <i.bardarov@gmail.com>
2014-11-26 13:01:08 +0200

    changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,11 @@ class Question
   include ActiveModel::Model
 
   attr_accessor :answer
+  attr_accessor :line1
+  attr_accessor :line2
+  attr_accessor :line3
+  attr_accessor :line4
+  attr_accessor :line5
   attr_accessor :question
   attr_accessor :start
   attr_accessor :step
@@ -17,6 +22,11 @@ class Question
     @session[@step] ||= {}
     super params if params
     @answer ||= @session[@step]['answer']
+    @line1 ||= @session[@step]['line1']
+    @line2 ||= @session[@step]['line2']
+    @line3 ||= @session[@step]['line3']
+    @line4 ||= @session[@step]['line4']
+    @line5 ||= @session[@step]['line5']
   end
 
   def persisted?
@@ -25,6 +35,13 @@ class Question
 
   def save session=nil
     @session[@step]['answer'] = self.answer
+
+    @session[@step]['line1'] = self.line1
+    @session[@step]['line2'] = self.line2
+    @session[@step]['line3'] = self.line3
+    @session[@step]['line4'] = self.line4
+    @session[@step]['line5'] = self.line5
+
     valid?
   end
 
