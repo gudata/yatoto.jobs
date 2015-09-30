65ccb32c59314804149515abacf129b36d9ca278
Ivaylo Bardarov <ivailo@empowerunited.com>
2015-09-30 13:19:23 +0300

    doc

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :administrators
   resources :designers
 
-
+  # https://github.com/cyu/rack-cors
   controller :secrets, path: '/secrets' do
-    match 'header', via: [:post, :options, :get]
+    match 'header', via: [  :post, :options, :get]
     match 'ajax_1', via: [:post, :options, :get]
     match 'ajax_2', via: [:post, :options, :get]
   end
