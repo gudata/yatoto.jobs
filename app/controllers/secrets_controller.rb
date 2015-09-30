c8928b4096eb8628258cef1200a61288dfc69ae0
Ivaylo Bardarov <ivailo@empowerunited.com>
2015-09-30 13:19:10 +0300

    no authtoken

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,6 +2,8 @@ class SecretsController < ApplicationController
   respond_to :html, :json
 
   # https://gist.github.com/dhoelzgen/cd7126b8652229d32eb4
+  # https://github.com/cyu/rack-cors <- maybe better
+  skip_before_filter :verify_authenticity_token
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
 
