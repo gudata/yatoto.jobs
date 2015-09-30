b7ed406ebca54dbaf4ff2562570ceaa634ddb215
Ivaylo Bardarov <ivailo@empowerunited.com>
2015-09-30 13:11:10 +0300

    move to match

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   resources :administrators
   resources :designers
 
-  resources :secrets do
-    collection do
-      get :header
-      get :ajax_1, via: [ :post, :options, :get]
-      get :ajax_2, via: [ :post, :options, :get]
-    end
+
+  controller :secrets, path: '/secrets' do
+    match 'header', via: [:post, :options, :get]
+    match 'ajax_1', via: [:post, :options, :get]
+    match 'ajax_2', via: [:post, :options, :get]
   end
 
 end
