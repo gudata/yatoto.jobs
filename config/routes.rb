2dfb9cedc969b0565479bc2c553061dcbbc6f8d1
Ivaylo Bardarov <ivailo@empowerunited.com>
2015-09-30 16:30:01 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # https://github.com/cyu/rack-cors
   controller :secrets, path: '/secrets' do
     match 'header', via: [  :post, :options, :get]
-    match 'ajax_1', via: [:post, :options, :get]
-    match 'ajax_2', via: [:post, :options, :get]
+    match 'ajax_1', via: [:post, :options, :get],  as: 'ajax_1_secrets'
+    match 'ajax_2', via: [:post, :options, :get],  as: 'ajax_2_secrets'
   end
 
 end
