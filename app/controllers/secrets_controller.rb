ab93293f8f565308f5420cb2bdb796675285a5ad
Ivaylo Bardarov <ivailo@empowerunited.com>
2015-09-30 15:09:07 +0300

    sync headers

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -10,7 +10,7 @@ class SecretsController < ApplicationController
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, X-Requested-With, Key'
+    headers['Access-Control-Allow-Headers'] = 'Accepts, Origin, Language, Encoding, Content-Type, Accept, Authorization, Token, X-Requested-With, Key'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
@@ -19,7 +19,8 @@ class SecretsController < ApplicationController
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       # headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, X-Requested-With, Key'
+      headers['Access-Control-Allow-Headers'] = 'Accepts, Origin, Language, Encoding, Content-Type, Accept, Authorization, Token, X-Requested-With, Key'
+
       headers['Access-Control-Max-Age'] = '1728000'
 
       render :text => '', :content_type => 'text/plain'
